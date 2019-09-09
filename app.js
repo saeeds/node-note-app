7f5f8c32de76f86bce6276ee70e37d1d3f533f6b
Saeed Alsharqawi <saed_sharqawi@hotmail.com>
2019-09-09 10:06:23 +0300

    Add yargs commands and Builder

--- app.js
+++ app.js
@@ -1,17 +1,65 @@
-const fs = require('fs');
-const getNotes =  require('./notes.js');
 const chalk = require('chalk');
-const msg = getNotes();
-var validator = require('validator');
+const yargs = require('yargs')
+const getNotes = require('./notes.js');
+//console.log(process.argv);
 
-console.log(chalk.green(msg));
-console.log(chalk.green('Success!'));
-console.log(validator.isEmail("saed_sharqawi@hotmail.com"));
-fs.writeFileSync('notes.txt', 'My name is saeed alsharqwi');
-fs.appendFileSync('notes.txt', ' data to append');
+//Customize yargs verion
+yargs.version('1.1.0');
+//create the add command
+yargs.command({
+   command: 'add',
+   describe: 'Add a new note',
+   builder: {
+    title: {
+        describe: 'Note title',
+        demandOption: true,
+        type: 'string'
+    },
+    body: {
+        describe: 'Note body',
+        demandOption: true,
+        type: 'string'
+    }
+},
+   handler: function(argv) {
+       console.log(`Title: ${argv.title} ,  Body:  ${argv.body}`);
+   }
+});
 
-const error = chalk.bold.red;
-const warning = chalk.keyword('orange');
+//create the remove command 
+yargs.command({
+   command: 'remove',
+   describe: 'Remove a note',
+   handler: function(){
+       console.log('Removing the note');
+   }
+});
 
-console.log(error('Error!'));
-console.log(warning('Warning!'));
+// Create list command
+yargs.command({
+    command: 'list',
+    describe: 'List your notes',
+    handler() {
+      console.log('listing out all note')
+    }
+})
+
+// Create read command
+yargs.command({
+    command: 'read',
+    describe: 'Read a note',
+    builder: {
+        title: {
+            describe: 'Note title',
+            demandOption: true,
+            type: 'string'
+        }
+    },
+    handler(argv) {
+       console.log('read a note')
+    }
+})
+
+yargs.parse();
+//add, remove, read, list
+console.log(yargs.argv);
